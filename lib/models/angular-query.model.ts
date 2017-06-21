ab481e21b2d0f184211cf74c807964275327e20e
Balázs Rostás <balazs.rostas@avatao.com>
2017-06-21 13:55:00 +0200

    Implemented Query send method

--- lib/models/angular-query.model.ts
+++ lib/models/angular-query.model.ts
@@ -1,7 +1,11 @@
 import {Collection, QueryJSON} from 'collection-json-base/interfaces';
 import {QueryBase} from 'collection-json-base/models';
+import {Http, RequestOptions, URLSearchParams} from '@angular/http';
+import {CollectionConfigurationManager} from 'collection-json-base';
 import {AngularCollection} from './angular-collection.model';
 import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/of';
 
 export class AngularQuery extends QueryBase {
 
@@ -10,6 +14,20 @@ export class AngularQuery extends QueryBase {
     }
 
     public send(): Observable<Collection> {
-        return new Observable<AngularCollection>();
+        const requestOptions = new RequestOptions();
+        const params = new URLSearchParams();
+
+        if (typeof this.dataStore !== 'undefined') {
+            for (const data of this.dataStore) {
+                if (typeof data.value !== 'undefined') {
+                    params.set(data.name, String(data.value));
+                }
+            }
+        }
+
+        requestOptions.params = params;
+
+        return CollectionConfigurationManager.getHttpService<Http>().get(this.href, requestOptions)
+            .map((result) => new AngularCollection(result.json().collection));
     }
 }
