e615aa8859ee463a6a11a4ac90112d794035740b
Balázs Rostás <balazs.rostas@avatao.com>
2017-06-23 14:48:34 +0200

    Implemented Validations Array extension checks

--- lib/models/angular-template.model.ts
+++ lib/models/angular-template.model.ts
@@ -31,6 +31,8 @@ export class AngularTemplate extends TemplateBase {
             throw new Error('Href must be specified to send a POST request using the template');
         }
 
+        this.validationsExtensionCheck();
+
         const body = { template: this.json() };
 
         return CollectionConfigurationManager.getHttpService<Http>().post(this.href, body)
@@ -39,8 +41,10 @@ export class AngularTemplate extends TemplateBase {
     public update(): Observable<Collection> {
 
         if (typeof this.href === 'undefined') {
-            throw new Error('Href must be specified to send a PUT request using the template');
+            throw new Error('Href must be specified to send a request using the template');
         }
+
+        this.validationsExtensionCheck();
 
         const body = { template: this.json() };
 
@@ -53,6 +57,103 @@ export class AngularTemplate extends TemplateBase {
 
         for (const data of dataArray) {
             this.dataStore.add(new AngularData(data));
+        }
+    }
+
+    private validationsExtensionCheck(): void {
+
+        let wasError = false;
+
+        for (const data of this.dataStore) {
+            if (typeof data.validations !== 'undefined') {
+                for (const validation of data.validations) {
+
+                    if (typeof validation.arguments === 'undefined') {
+                        continue;
+                    }
+
+                    switch (validation.name) {
+                        case 'inclusion':
+                            // Filter the arguments array where the value equals the current value, if empty error is thrown
+                            if (typeof data.value !== 'undefined') {
+                                wasError = validation.arguments.filter(argument => argument.value === data.value).length === 0;
+                            }
+                            break;
+                        case 'exclusion':
+                            // Filter the arguments array where the value equals the current value, if not empty error is thrown
+                            if (typeof data.value !== 'undefined') {
+                                wasError = validation.arguments.filter(argument => argument.value === data.value).length > 0;
+                            }
+                            break;
+                        case 'format':
+                            if (typeof data.value !== 'undefined') {
+                                const regexp_argument = validation.arguments.find(argument => argument.name === 'regex');
+
+                                if (typeof regexp_argument === 'undefined') {
+                                    break;
+                                }
+
+                                wasError = !RegExp(String(regexp_argument.value)).test(String(data.value));
+                            }
+                            break;
+                        case 'length':
+                            if (typeof data.value !== 'undefined') {
+                                const lower_bound_argument = validation.arguments.find(argument => argument.name === 'lower_bound');
+                                const upper_bound_argument = validation.arguments.find(argument => argument.name === 'upper_bound');
+
+                                if (typeof lower_bound_argument === 'undefined' || typeof upper_bound_argument === 'undefined' ) {
+                                    break;
+                                }
+
+                                const lower_bound = Number(lower_bound_argument.value);
+                                const upper_bound = Number(upper_bound_argument.value);
+                                const valueNumber = Number(data.value);
+
+                                if (!(valueNumber > lower_bound && valueNumber < upper_bound)) {
+                                    wasError = true;
+                                }
+                            }
+                            break;
+                        case 'file_type':
+                            // Expecting value to be Data URI with mime types
+                            if (typeof data.value !== 'undefined') {
+                                const fileType = (<string>data.value).split(';')[0].split(':')[1];
+                                wasError = validation.arguments.filter(
+                                    argument => (<string>argument.value).includes(fileType)).length === 0;
+                            }
+                            break;
+                        case 'file_size':
+                            // Expecting value to be Data URI with mime types
+                            if (typeof data.value !== 'undefined') {
+                                // Base64 encoding turns 6 bytes into 8, so we multiply by 3/4
+                                const fileSize = (<string>data.value).length * (3 / 4);
+                                const lower_bound_argument = validation.arguments.find(argument => argument.name === 'lower_bound');
+                                const upper_bound_argument = validation.arguments.find(argument => argument.name === 'upper_bound');
+
+                                if (typeof lower_bound_argument === 'undefined' || typeof upper_bound_argument === 'undefined' ) {
+                                    break;
+                                }
+
+                                const lower_bound = Number(lower_bound_argument.value);
+                                const upper_bound = Number(upper_bound_argument.value);
+
+                                if (!(fileSize > lower_bound && fileSize < upper_bound)) {
+                                    wasError = true;
+                                }
+                            }
+                            break;
+                        case 'presence':
+                            if (typeof data.validations === 'undefined') {
+                                wasError = true;
+                            }
+                            break;
+                    }
+
+                    if (wasError) {
+                        throw new Error(validation.message || 'Validation failed');
+                    }
+                }
+            }
         }
     }
 }
