b4653cc2e61ce19fa7d477e9305728c65a5372ad
Balázs Rostás <balazs.rostas@avatao.com>
2017-07-04 16:30:22 +0200

    AngularLink's follow method now accepts query parameters

--- lib/models/angular-link.model.ts
+++ lib/models/angular-link.model.ts
@@ -1,6 +1,6 @@
 import {LinkJSON} from 'collection-json-base/interfaces';
 import {LinkBase, CollectionConfigurationManager} from 'collection-json-base/models';
-import {Http} from '@angular/http';
+import {Http, RequestOptions, URLSearchParams} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
@@ -12,9 +12,20 @@ export class AngularLink extends LinkBase {
         super(link);
     }
 
-    public follow(): Observable<AngularCollection> {
+    public follow(params?: Map<string, string>): Observable<AngularCollection> {
+
+        const requestOptions = new RequestOptions();
+
+        if (typeof params !== 'undefined') {
+            const urlParams = new URLSearchParams();
+            for (const [key, value] of params.entries()) {
+                urlParams.set(key, value);
+            }
+            requestOptions.params = urlParams;
+        }
+
         return CollectionConfigurationManager.getHttpService<Http>()
-            .get(this.href)
+            .get(this.href, requestOptions)
             .map((response) => new AngularCollection(response.json().collection));
     }
 }
